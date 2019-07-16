f61b9eb6b8606b25c5c68d858deaa0341bf1c3d1
huyong007 <1341696804@qq.com>
2019-07-16 08:59:06 +0800

    this direction

--- es6/main.js
+++ es6/main.js
@@ -1,5 +1,37 @@
-export class q {
-    constructor() {
-        this.es6 = 'hello';
-    }
+var Point = function (x, y) {
+    console.log(x, y);
 }
+
+Point.prototype.toString = function () {
+
+}
+
+Object.keys(Point.prototype);
+
+
+
+
+class Lines {
+    constructor() {
+        this.x = y;
+        this.y = x;
+    }
+    toString() {
+        return this.x, this.y;
+    }
+
+}
+
+console.log(
+    Object.keys(Point.prototype),'\n',
+    'Point','\n',
+    Object.keys(Lines.prototype),'\n',
+    "Lines",'\n',
+);
+
+console.log(
+    Object.getOwnPropertyNames(Point.prototype),'\n',
+    'Point','\n',
+    Object.getOwnPropertyNames(Lines.prototype),'\n',
+    "Lines",'\n',
+)
