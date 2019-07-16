424d016305ef6995e212021b0a7f7d9598abed4b
huyong007 <1341696804@qq.com>
2019-07-16 20:56:23 +0800

    new target

--- es6/main.js
+++ es6/main.js
@@ -1,37 +1,8 @@
-var Point = function (x, y) {
-    console.log(x, y);
-}
-
-Point.prototype.toString = function () {
-
-}
-
-Object.keys(Point.prototype);
-
-
-
-
-class Lines {
-    constructor() {
-        this.x = y;
-        this.y = x;
+function Person(name){
+    if(name.target===Person){
+        this.name = name;
+    }else{
+        console.log('wrong')
+        // new target属性
     }
-    toString() {
-        return this.x, this.y;
-    }
-
 }
-
-console.log(
-    Object.keys(Point.prototype),'\n',
-    'Point','\n',
-    Object.keys(Lines.prototype),'\n',
-    "Lines",'\n',
-);
-
-console.log(
-    Object.getOwnPropertyNames(Point.prototype),'\n',
-    'Point','\n',
-    Object.getOwnPropertyNames(Lines.prototype),'\n',
-    "Lines",'\n',
-)
