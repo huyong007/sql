066c28360387be464aabe9bc2cfd75dce393e5b6
huyong007 <1341696804@qq.com>
2019-07-17 09:04:15 +0800

    class prototype and _proto_

--- es6/main.js
+++ es6/main.js
@@ -1,8 +1,22 @@
-function Person(name){
-    if(name.target===Person){
-        this.name = name;
-    }else{
-        console.log('wrong')
-        // new target属性
+class Point {
+    constructor() {
+        this.p = 'p';
+    }
+    print() {
+        console.log(this.x, this.y)
     }
 }
+Point.prototype.n = 'n';
+class ColorPoint extends Point {
+    constructor() {
+        super();
+        this.p = 'q';
+    }
+    get m() {
+        console.log(super.p, super.n, 'super.p super.m');
+    }
+}
+let cp = new ColorPoint();
+cp.m;
+
+
