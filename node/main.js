3f64d137d4e519da76ba602ea45ad68ad37c846d
huyong007 <1341696804@qq.com>
2019-07-20 20:59:45 +0800

    5 class and liaoxuefeng http and react demo01

--- node/main.js
+++ node/main.js
@@ -2,11 +2,27 @@
 
 var fs = require('fs');
 
-fs.readFile('sample.txt', 'utf-8', function (err, data) {
-    if (err) {
-        console.log(123, '123');
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-});
+var ws1 = fs.createWriteStream('output1.txt', 'utf-8');
+
+ws1.write('使用stream写入文本数据');
+
+ws1.write('END.');
+
+ws1.end();
+
+var ws2 = fs.createReadStream('output2.txt');
+
+// ws2.write(new Buffer('使用Stream写入二进制数据。。。\n', 'utf-8'));
+
+// ws2.write(new Buffer('END', 'utf-8'));
+
+// ws2.end();
+
+var rs = fs.createReadStream('sample.txt');
+
+var ws = fs.createWriteStream('copied.txt');
+
+rs.pipe(ws, { end: false });
+
+
+
