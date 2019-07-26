008fbfe0ba09ee580c825e7fa43ab94666ec7d8c
huyong007 <1341696804@qq.com>
2019-07-26 08:58:20 +0800

    obj deconstruct

--- node/app.js
+++ node/app.js
@@ -1,8 +1,17 @@
-(function (a) {
-    var a = 10;
-    console.log(a, 'a');
+function* fibs() {
+    let a = 0;
+    let b = 1;
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b];
+        console.log(a, b, 'a,b');
 
-}());
+    }
+}
 
+let [first, second, third, fourth, fifth, sixth] = fibs();
 
-console.log(a, 'a 外部');
+console.log(sixth);
+
+console.log([1,2,3][4],'10');
+
