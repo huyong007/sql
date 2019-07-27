241aff03237cfa755ec85fb1fa47ec8de28eb6a2
huyong007 <1341696804@qq.com>
2019-07-27 09:01:43 +0800

    function arguments  deconstruct

--- node/app.js
+++ node/app.js
@@ -1,17 +1,6 @@
-function* fibs() {
-    let a = 0;
-    let b = 1;
-    while (true) {
-        yield a;
-        [a, b] = [b, a + b];
-        console.log(a, b, 'a,b');
+let { x, y = 5 } = { x: 1 };
 
-    }
-}
+console.log(x, y, 'x,y');
 
-let [first, second, third, fourth, fifth, sixth] = fibs();
 
-console.log(sixth);
-
-console.log([1,2,3][4],'10');
-
+[[1, 2], [3, 4]].map(([a, b]) => console.log(a + b));
