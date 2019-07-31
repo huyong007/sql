76ba09131fdcaa94fa7c01e4f566642c4766d8bf
huyong007 <1341696804@qq.com>
2019-07-31 09:04:20 +0800

    arguments location

--- node/app.js
+++ node/app.js
@@ -1,4 +1,12 @@
-for (let key of 'first') {
-    console.log(key, 'key');
+var x = 100;
+
+function calculate(p = x + 1) {
+    console.log(p, 'p');
 
 }
+
+
+calculate();
+
+var  x = 20;
+calculate();
