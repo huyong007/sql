65e54e5b2b84fe7afc8b488180e5c47b933e0d8a
huyong007 <1341696804@qq.com>
2019-08-26 09:00:53 +0800

    算法：排序数组对象

--- algorithms/first-chart.js
+++ algorithms/first-chart.js
@@ -23,30 +23,27 @@ for (let j = array.length - 1; j >= 0; j--) {
 
 // 冒泡排序算法
 
-let b = [1, 9, 4, 8, 7];
-let arr = [1, 4, 9, 5, 2]
-function bubble(arr) {
-    let t;
-    let a = [];
-    let n = arr.length;
-    for (let i = 1; i <= n; i++) {
-        a[i] = arr[i - 1];
-    }
-    console.log(a, 'a');
+let scoreName = [
+    {},
+    { name: 'huhu', score: 5 },
+    { name: 'haha', score: 3 },
+    { name: 'xixi', score: 5 },
+    { name: 'hengheng', score: 2 },
+    { name: 'gaoshou', score: 8 },
+];
 
-    for (let i = 1; i <= n - 1; i++) {
-        for (let j = 1; j <= n - i; j++) {
-            console.log(a[j + 1], 'a[j+1]');
-            if (a[j] < a[j + 1]) {
-                t = a[j]; a[j] = a[j + 1]; a[j + 1] = t;
-            }
+let length = scoreName.length;
+
+for (let i = 1; i <= length - 1; i++) {
+    for (let j = 1; j <= length - i; j++) {
+        if (scoreName[j].score < scoreName[j + 1].score) {
+            t = scoreName[j]; scoreName[j] = scoreName[j + 1]; scoreName[j + 1] = t;
         }
-    }
-
-    for (let i = 1; i <= n; i++) {
-        console.log(a[i], 'a[i]');
     }
 }
 
-bubble(arr);
-bubble(b);
+
+for (let i = 1; i < -n; i++) {
+    console.log(scoreName[i].name, 'scoreName.[i].name');
+
+}
