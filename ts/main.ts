90cdb9dd7ec666648bc5d3ec590b84943f61e686
huyong007 <1341696804@qq.com>
2019-09-03 09:01:07 +0800

    ts：慕课网一个小时

--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,19 @@
-class Main {
-    static girl: string = "horny girl"
+function* getStockPrice(stock: string) {
+while (true) {
+    yield Math.random() * 100;
 }
+}
+
+var priceGenerator = getStockPrice("IBM");
+
+var limitPrice = 15;
+
+var price = 100;
+
+while (price > limitPrice) {
+price = priceGenerator.next().value;
+console.log(`the generator return ${price}`);
+
+}
+
+console.log(`buying at ${price}`);
