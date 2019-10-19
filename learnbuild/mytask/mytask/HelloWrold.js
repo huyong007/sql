2eed1ffbe42d20a3d615ed602d196cdbd2596154
huyong007 <1341696804@qq.com>
2019-10-19 11:14:48 +0800

    learn build task

---
+++ learnbuild/mytask/mytask/HelloWrold.js
@@ -0,0 +1,6 @@
+"use strict";
+function sayHello(name) {
+    console.log("hello " + name);
+}
+sayHello("huyong");
+var school = 'shangqiu';
---
+++ learnbuild/mytask/mytask/HelloWrold.ts
@@ -0,0 +1,8 @@
+function sayHello(name: string): void {
+    console.log(`hello ${name}`);
+
+}
+
+sayHello("huyong");
+
+let school:string = 'shangqiu';
