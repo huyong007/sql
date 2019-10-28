4a993eda9d862cfe092bf707dbb618785e25955f
huyong007 <1341696804@qq.com>
2019-10-28 08:56:54 +0800

    算法：递归

--- algorithms/a.js
+++ algorithms/a.js
@@ -1,9 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+    let arrList = [];
+    function generate(left, right, n, s) {
+        if (left === n && right === n) {
+            arrList.push(s);
+            console.log(s, left, right, 's2');
+            return;
+        }
+        console.log(s, left, right, 's1');
 
+        if (left < n) generate(left + 1, right, n, s + '(');
+        if (left > right) generate(left, right + 1, n, s + ')');
+    }
+    console.log('s3');
+    generate(0, 0, n, '');
+    return arrList;
+};
 
-// s[i].charCodeAt()-aCode
-let s = 'abcdefg';
-let aCode = 'a'.charCodeAt(0);
-console.log(aCode,'aCode');
-console.log(s.charCodeAt(0),'charAt');
-console.log(s.charCodeAt(1),'charAt');
-console.log(s.charCodeAt(2),'charAt');
+generateParenthesis(3);
