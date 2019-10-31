ff0d0e5b13289fa15cac274071a8a289031ea190
huyong007 <1341696804@qq.com>
2019-10-31 09:07:20 +0800

    算法

--- algorithms/a.js
+++ algorithms/a.js
@@ -1,14 +1,8 @@
 /**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
  */
-var reverseString = function (s) {
-    let left = 0, right = s.length - 1;
-    while (left < right) {
-        [s[left], s[right]] = [s[right], s[left]];
-        left++;
-        right--;
-    }
-    return s;
+var myPow = function(x, n) {
+    
 };
-reverseString(["h", "e", "l", "l", "o"]);
