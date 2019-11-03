149b195492d1ed79997a63b7de12e78a3479ac70
huyong007 <1341696804@qq.com>
2019-11-03 22:06:53 +0800

    algorithms:bfs,dfs,二分查找，贪心算法

---
+++ algorithms/Week-03/BFS代码模版.md
@@ -0,0 +1,21 @@
+
+> BFS 代码模版
+```python
+
+def BFS(graph, start, end):
+
+	queue = [] 
+	queue.append([start]) 
+	visited.add(start)
+
+	while queue: 
+		node = queue.pop() 
+		visited.add(node)
+
+		process(node) 
+		nodes = generate_related_nodes(node) 
+		queue.push(nodes)
+
+	# other processing work 
+	...
+```
--- algorithms/test.js
+++ algorithms/test.js
@@ -1,15 +1,33 @@
 
 
-var generateParentthesis = function (n) {
-    let arrlist = [];
-    function generate(left, right, n, s) {
-        if (left === n && right === n) {
-            arrlist.push(s);
-            return;
-        }
-        if (left < n) generate(left + 1, right, n, s + ')');
-        if (left > right) generate(left, right + 1, n, s + '(');
-    }
-    generate(0, 0, n, '');
-    return arrlist;
+// var generateParentthesis = function (n) {
+//     let arrlist = [];
+//     function generate(left, right, n, s) {
+//         if (left === n && right === n) {
+//             arrlist.push(s);
+//             return;
+//         }
+//         if (left < n) generate(left + 1, right, n, s + ')');
+//         if (left > right) generate(left, right + 1, n, s + '(');
+//     }
+//     generate(0, 0, n, '');
+//     return arrlist;
+// }
+
+function subsets(nums) {
+	const powerset = [];
+	generatePowerset([], 0);
+
+	function generatePowerset(path, index) {
+        console.log(path,index,'path,index');
+        
+		powerset.push(path);
+		for (let i = index; i < nums.length; i++) {
+			generatePowerset([...path, nums[i]], i + 1);
+		}
+	}
+
+	return powerset;
 }
+
+subsets([1, 2, 3]);
