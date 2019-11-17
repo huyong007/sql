a1d5f70bc67f329df634ecc58da6d41ef9a563bb
huyong007 <1341696804@qq.com>
2019-11-17 18:29:10 +0800

    算法

--- algorithms/a.js
+++ algorithms/a.js
@@ -1,18 +1,40 @@
-/**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
- */
-var reverseString = function (s) {
-    let midLevel = s.length % 2 ? Math.floor((s.length / 2)) : s.length / 2;
-
-    function recurFun(s, level) {
-        if (level === midLevel) {
-            return s;
-        }
-        [s[level, s[s.length - 1 - level]]] = [s[s.length - 1 - level], s[level]];
-        if (level < midLevel) return recurFun(s, level + 1);
+var longestCommonSubsequence = function (text1, text2) {
+    let mArray = text1.split('');
+    let nArray = text2.split('');
+    let dpArray = new Array(nArray.length);
+    for (let index = 0; index < dpArray.length; index++) {
+        dpArray[index] = new Array(mArray.length).fill(0);
     }
-    return recurFun(s, 0);
+    nArray[0] === mArray[0] ? dpArray[0][0] = 1 : dpArray[0][0] = 0;
+    for (let i = 1; i < mArray.length; i++) {
+        if (nArray[0] === mArray[i]) {
+            dpArray[0][i] = dpArray[0][i - 1] + 1;
+        } else {
+            dpArray[0][i] = dpArray[0][i - 1];
+        }
+    }
+    for (let i = 1; i < nArray.length; i++) {
+        if (mArray[0] === nArray[i]) {
+            dpArray[i][0] = dpArray[i - 1][0] + 1;
+        } else {
+            dpArray[i][0] = dpArray[i - 1][0];
+        }
+    }
+    console.log(nArray, mArray, dpArray, 'nArray,mArray,dpArray');
+
+    for (let i = 1; i < nArray.length; i++) {
+        for (let j = 1; j < mArray.length; j++) {
+            if (mArray[j] === nArray[i]) {
+                dpArray[i][j] = Math.max(dpArray[i - 1][j], dpArray[i][j - 1]) + 1;
+            } else {
+                dpArray[i][j] = Math.max(dpArray[i - 1][j], dpArray[i][j - 1]);
+            }
+        }
+    }
+    console.log(dpArray, 'dpArray');
+
+    return dpArray[nArray.length - 1][mArray.length - 1];
+
 };
 
-reverseString(["h", "e", "l", "l", "o"]);
+longestCommonSubsequence("bsbininm", "jmjkbkjkv");
