736c0b4870a6eac9746d81588e0542dee40a3df5
huyong007 <1341696804@qq.com>
2019-12-03 22:53:04 +0800

    learn node

--- node/app.js
+++ node/app.js
@@ -1,5 +1,18 @@
-let array = [];
-for (let i = 0; i < 181; i++) {
-    array.push((50 + i) + '');
-};
-console.log(array);
+var playerAction = process.argv[2];
+console.log(playerAction);
+if (
+  playerAction != "rock" &&
+  playerAction != "paper" &&
+  playerAction != "scissor"
+) {
+  console.log("请输入rock或paperhuoscissor");
+} else {
+  var computerAction;
+  var random = Math.random() * 3;
+  if (random < 1) {
+    computerAction = "rock";
+    console.log("电脑出了石头");
+  } else if (random > 2) {
+    // computerAction =
+  }
+}
